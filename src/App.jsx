af44e45565daf14295023d70da682a9aefb07166
venomcodes0943 <dhcodes0943@gmail.com>
2024-03-28 21:45:09 +0500

    React With Js

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,17 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="w-full h-screen pt-6 bg-slate-900 text-white">
+        <h1 className='text-3xl text-center font-bold bg-slate-400 rounded-md mx-4 py-2'>Welcome To my React</h1>
+        <h2 className='text-center pt-5 text-2xl'>This Is useState: {count}</h2>
+        <center className='py-5'>
+          <button className="px-4 rounded-md py-2 text-1xl font-bold bg-sky-500 outline-none" onClick={() => setCount(count + 1)}>Click Me</button>
+        </center>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
