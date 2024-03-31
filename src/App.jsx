2b1279784da296186f7d1804c6b16aa5bb46ef6e
venomcodes0943 <dhcodes0943@gmail.com>
2024-03-31 17:43:20 +0500

    on Windows Too

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,7 @@ function App() {
 
   return (
     <>
-      <div className="w-full h-screen pt-6 bg-slate-900 text-white">
-        <h1 className='text-3xl text-center font-bold bg-slate-400 rounded-md mx-4 py-2'>Welcome To my React</h1>
-        <h2 className='text-center pt-5 text-2xl'>This Is useState: {count}</h2>
-        <center className='py-5'>
-          <button className="px-4 rounded-md py-2 text-1xl font-bold bg-sky-500 outline-none" onClick={() => setCount(count + 1)}>Click Me</button>
-        </center>
-      </div>
+      
     </>
   )
 }
